5b0ca04c714348293b72aaad55f80573500ff2b5
zan.korosak <zan.korosak@xlab.si>
2023-01-11 20:28:35 +0100

    Final push.

--- pyboolnet/py_run_network_analysis.py
+++ pyboolnet/py_run_network_analysis.py
@@ -1,37 +1,60 @@
 import pyboolnet
 from pyboolnet.repository import get_primes
 from pyboolnet.file_exchange import bnet2primes
+from pyboolnet.file_exchange import bnet2primes
 from pyboolnet.interaction_graphs import primes2igraph, igraph2image, local_igraph_of_state, add_style_interactionsigns
 from pyboolnet.state_space import random_state
 from pyboolnet.phenotypes import compute_phenotypes, compute_phenotype_diagram, create_phenotypes_piechart
-from pyboolnet.attractors import compute_attractors
+from pyboolnet.attractors import compute_attractors, create_attractor_report
 from pyboolnet.state_transition_graphs import create_stg_image
+from pyboolnet.basins_of_attraction import weak_basin, strong_basin, cycle_free_basin
+import json
 
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 im_dir = './images/'
 
-bnet = """
-    v1,    !v2
-    v2,    !v3
-    v3,    !v1
-"""
 
-primes = bnet2primes(bnet)
-primes = get_primes("remy_tumorigenesis")
+def reprissilator():
+    bnet = """
+        v1,    !v2
+        v2,    !v3
+        v3,    !v1
+    """
+    primes = bnet2primes(bnet)
+    igraph = primes2igraph(primes)
+
+    attractors = compute_attractors(primes, update='asynchronous', fname_json=im_dir + "attractors_repr.json")
+    state = attractors["attractors"][0]["state"]["str"]
+
+    weak = weak_basin(primes, "asynchronous", state)
+    for key, value in weak.items():
+        print(f"{key} = {value}")
+
+    igraph2image(igraph, im_dir + "interaction_graph.pdf")
+
+    create_stg_image(primes, update="asynchronous", fname_image=im_dir + "state_trans_graph.pdf")
+    markers = ["WOX", "MGP"]
+
+    phenos = compute_phenotypes(attractors, markers)
+
+    compute_phenotype_diagram(phenos, fname_image=im_dir + "phenotype_diagram.pdf")
 
 
-print(primes)
+# Load the prostate cancer network
+def prostate_cancer():
+    # Read the text file and convert it to a primes dictionary
+    # prost_cancer_primes = bnet2primes("./data/Montagud2021_Prostate_Cancer.bnet")
 
-"""
-create_stg_image(primes, update="asynchronous", fname_image=im_dir + "igraph.pdf")
-create_stg_image(primes, update="asynchronous", fname_image=im_dir + "igraph2.pdf", styles=["anonymous", "sccs"])
+    primes = {}
+    with open("data/Montagud2021_Prostate_Cancer_primes.json") as f:
+        primes = json.load(f)
 
-igraph = primes2igraph(primes)
-for x in igraph.nodes:
-    if "GF" in x:
-        igraph.nodes[x]["shape"] = "square"
-        igraph.nodes[x]["fillcolor"] = "lightblue"
+    #igraph = primes2igraph(primes)
 
-igraph2image(igraph, im_dir + "igraph3.pdf")
+    #attractors = compute_attractors(primes, update='asynchronous', fname_json=im_dir + "cancer_attr.json")
 
-print(igraph.nodes(), igraph.edges())
-"""
+    create_attractor_report(primes, "cancer_attr_report.txt")
+
+prostate_cancer()
